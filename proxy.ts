7c317334faa8ff6d899494360889fdaa2ed237b9
Yilmaz Ayranci <yilliy@easyconcepts.com>
2026-06-19 18:42:28 +0200

    Update proxy matcher for next-intl

--- proxy.ts
+++ proxy.ts
@@ -4,5 +4,5 @@ import { routing } from "@/i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ["/", "/(nl|en|tr)/:path*"],
+  matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)"],
 };
