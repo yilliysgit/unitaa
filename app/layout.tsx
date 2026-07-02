87525126682373ed12d5f05528a5f6242d341080
Yilmaz Ayranci <yilliy@easyconcepts.com>
2026-07-02 22:34:24 +0200

    Add Google Tag Manager binnen body

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import "./globals.css";
-
-import Script from 'next/script';
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Unit AA — Administratie & Advies",
@@ -20,16 +19,10 @@ export default function RootLayout({
     <html lang="nl">
       <body className="min-h-full flex flex-col">
         {children}
-<Script id="gtm" strategy="afterInteractive">
-  {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-WJ3BLV3Q');
-  `}
-</Script>        
       </body>
+      
+      <GoogleTagManager gtmId="GTM-WJ3BLV3Q" />
+
     </html>
   );
 }
